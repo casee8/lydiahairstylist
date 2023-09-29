bc0c07cd463cd3ba8b5fe344a2b7ef168edf0787
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-09-29 15:31:15 +0100

    Add mobile menu + functionality

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const bookingBtn = document.querySelector("#bookingBtn");
 const bookingField = document.querySelector("#sbw_45sh9i");
-
 const pointers = document.querySelectorAll(".pointer");
-
-function toggleBtn() {
-  bookingField.classList.toggle("not-visible");
-}
+const menuBtn = document.querySelector("#menu-btn");
 
 // Add a click event listener to each pointer element
 pointers.forEach((pointer) => {
   pointer.addEventListener("click", () => {
     // Get the price details element contained within the pointer element
     const priceDetailsElement = pointer.querySelector(".price-details");
-
+    
     // Toggle the visibility of the price details element.
     priceDetailsElement.classList.toggle("not-visible");
   });
+});
+
+menuBtn.addEventListener("click", () => {
+  const mobileMenu = document.querySelector("#mobile-menu");
+  mobileMenu.classList.toggle("hidden");
 });

2618561e10145576004a56524037abb91748de88
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-09-30 23:31:54 +0100

    Reorganize files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,13 @@
-const bookingBtn = document.querySelector("#bookingBtn");
-const bookingField = document.querySelector("#sbw_45sh9i");
-const pointers = document.querySelectorAll(".pointer");
+const serviceGroups = document.querySelectorAll(".service-groups");
 const menuBtnCont = document.querySelector("#menu-btn-container");
 const menuBtn = document.querySelector("#menu-btn");
 const isMobileMenuOpen = false;
 
 // Add a click event listener to each pointer element
-pointers.forEach((pointer) => {
-  pointer.addEventListener("click", () => {
+serviceGroups.forEach((serviceGroup) => {
+  serviceGroup.addEventListener("click", () => {
     // Get the price details element contained within the pointer element
-    const priceDetailsElement = pointer.querySelector(".price-details");
+    const priceDetailsElement = serviceGroup.querySelector(".price-details");
 
     // Toggle the visibility of the price details element.
     priceDetailsElement.classList.toggle("not-visible");
@@ -20,8 +18,8 @@ menuBtnCont.addEventListener("click", () => {
   const mobileMenu = document.querySelector("#mobile-menu");
   mobileMenu.classList.toggle("hidden");
   if (!mobileMenu.classList.contains("hidden")) {
-    menuBtn.src = "./assets/svg/close.svg";
+    menuBtn.src = "./assets/media/svg/close.svg";
   } else {
-    menuBtn.src = "./assets/svg/menu.svg";
+    menuBtn.src = "./assets/media/svg/menu.svg";
   }
 });

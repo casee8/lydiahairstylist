db37459d0531b98b0f8a72b9a0a5efeba5cb1584
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-10-04 22:20:18 +0100

    Add some animations to the menus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,17 @@
 const serviceGroups = document.querySelectorAll(".service-groups");
 const menuBtnCont = document.querySelector("#menu-btn-container");
 const menuBtn = document.querySelector("#menu-btn");
-const isMobileMenuOpen = false;
 
 // Add a click event listener to each pointer element
 serviceGroups.forEach((serviceGroup) => {
   serviceGroup.addEventListener("click", () => {
     // Get the price details element contained within the pointer element
     const priceDetailsElement = serviceGroup.querySelector(".price-details");
+    const openCloseMarker = serviceGroup.querySelector("img");
+
+    // Animate menu status marker
+    // openCloseMarker.style = "transform: rotate(180deg); animation: openPos 0.2s;";
+    openCloseMarker.classList.toggle("openPosDetails");
 
     // Toggle the visibility of the price details element.
     priceDetailsElement.classList.toggle("not-visible");

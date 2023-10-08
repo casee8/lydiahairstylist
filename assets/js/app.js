3ee435ec3613d3ee8dce8e639ed4abcd7ca8be49
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-10-08 10:04:08 +0100

    Hide menu if there is a click on something else

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 const serviceGroups = document.querySelectorAll(".service-groups");
+const mobileMenu = document.querySelector("#mobile-menu");
 const menuBtnCont = document.querySelector("#menu-btn-container");
 const menuBtn = document.querySelector("#menu-btn");
+const mainElement = document.querySelector("main");
+const footerElement = document.querySelector("footer");
+let isMenuOpen = false;
 
 // Add a click event listener to each pointer element
 serviceGroups.forEach((serviceGroup) => {
@@ -18,12 +22,31 @@ serviceGroups.forEach((serviceGroup) => {
   });
 });
 
+// Swap menu btn depending on it's position (opened, closed)
 menuBtnCont.addEventListener("click", () => {
-  const mobileMenu = document.querySelector("#mobile-menu");
   mobileMenu.classList.toggle("hidden");
   if (!mobileMenu.classList.contains("hidden")) {
     menuBtn.src = "./assets/media/svg/close.svg";
+    isMenuOpen = true;
   } else {
+    menuBtn.src = "./assets/media/svg/menu.svg";
+    isMenuOpen = false;
+  }
+});
+
+// Hide menu if the user click on something which is not in the menu
+// Hide menu if main content is clicked
+mainElement.addEventListener("click", () => {
+  if (isMenuOpen) {
+    mobileMenu.classList.add("hidden");
+    menuBtn.src = "./assets/media/svg/menu.svg";
+  }
+});
+
+// Hide menu if the footer is clicked
+footerElement.addEventListener("click", () => {
+  if (isMenuOpen) {
+    mobileMenu.classList.add("hidden");
     menuBtn.src = "./assets/media/svg/menu.svg";
   }
 });

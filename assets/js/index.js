b4e7fec3c1fa5d9ebce78c0c8ed4c5d2f59dbb95
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-11-01 18:30:41 +0000

    Hide modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,19 @@
 const cookieBtn = document.querySelector("#cookie-btn");
-const modal = document.querySelector(".modal");
-const modalCloseBtn = document.querySelector(".modal-close-btn");
+// const modal = document.querySelector(".modal");
+// const modalCloseBtn = document.querySelector(".modal-close-btn");
 
 // Toggle cookie note
 cookieBtn.addEventListener("click", () => {
   document.querySelector(".cookie-desc").classList.toggle("hidden-desktop");
 });
 
-setTimeout(showModal, 500);
+// setTimeout(showModal, 500);
 
-function showModal() {
-  modal.classList.remove("hidden-desktop");
-}
+// function showModal() {
+//   modal.classList.remove("hidden-desktop");
+// }
 
 // Open - close modal, cookie
-modalCloseBtn.addEventListener("click", () => {
-  modal.classList.add("hidden-desktop");
-});
+// modalCloseBtn.addEventListener("click", () => {
+//   modal.classList.add("hidden-desktop");
+// });

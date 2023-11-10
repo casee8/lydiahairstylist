b19836d16e2e3b044ea3b1d26e4a5038d3a6aacc
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-11-10 22:08:36 +0000

    Update cookie btn behavior

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,28 @@
+const htmlBody = document.querySelector("body");
 const cookieBtn = document.querySelector("#cookie-btn");
+const cookieDesc = document.querySelector(".cookie-desc");
+let isCookieDescOpen = false;
 // const modal = document.querySelector(".modal");
 // const modalCloseBtn = document.querySelector(".modal-close-btn");
 
-// Toggle cookie note
-cookieBtn.addEventListener("click", () => {
-  document.querySelector(".cookie-desc").classList.toggle("hidden-desktop");
+// Toggle cookie note if the user clicks on the cookie btn
+cookieBtn.addEventListener("click", (Event) => {
+  if (!isCookieDescOpen) {
+    cookieDesc.classList.remove("hidden-desktop");
+    isCookieDescOpen = true;
+  } else {
+    cookieDesc.classList.add("hidden-desktop");
+    isCookieDescOpen = false;
+  }
+  Event.stopPropagation();
+});
+
+// Hide cookie note if the user clicks anywhere on the page
+htmlBody.addEventListener("click", () => {
+  if (isCookieDescOpen) {
+    cookieDesc.classList.add("hidden-desktop");
+    isCookieDescOpen = false;
+  }
 });
 
 // setTimeout(showModal, 500);
